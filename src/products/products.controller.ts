5c4260bde8687cd8662647f2f27aa058928fcaa1
Joanna Lew/javascript-blog.git <lewjoanna96@gmail.com>
2023-08-10 13:11:06 +0200

    add delete endpoint, add orders, add create products

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Delete, Post, Body } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { CreateProductDTO } from './dtos/create-product.dto';
 
 @Controller('products')
 export class ProductsController {
@@ -8,4 +9,21 @@ export class ProductsController {
   getAll(): any {
     return this.productsService.getAll();
   }
+
+  @Get('/:id')
+  public getById(@Param('id') id: string) {
+    return this.productsService.getById(id);
+  }
+
+  @Delete('/:id')
+  deleteById(@Param('id') id: string){
+    this.productsService.deleteByID(id);
+    return { success: true};
+  }
+
+  @Post('/')
+  create(@Body() productData: CreateProductDTO) {
+    return this.productsService.create(productData);
+  }
+
 }
